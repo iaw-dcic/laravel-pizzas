02e4aaea45889f785da27c28b2a4326cad37af53
Mariano Tucat <mariano@skedgo.com>
2017-05-08 23:51:44 -0300

    update

--- database/seeds/PizzasSeeder.php
+++ database/seeds/PizzasSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Pizza;
 
 class PizzasSeeder extends Seeder {
 
@@ -8,17 +9,16 @@ class PizzasSeeder extends Seeder {
     {
         DB::table('pizzas')->delete();
 
-        Pizzas::create(['nombre' => 'Jamón y Queso', 'precio' => '75', 'tipo' => 'clásica']);
-        Pizzas::create(['nombre' => 'Fugazzeta', 'precio' => '85', 'tipo' => 'clásica']);
-        Pizzas::create(['nombre' => 'Muzzarela', 'precio' => '90', 'tipo' => 'clásica']);
-        Pizzas::create(['nombre' => 'Napolitana', 'precio' => '95', 'tipo' => 'clásica']);
-        Pizzas::create(['nombre' => 'Roquefort', 'precio' => '85', 'tipo' => 'clásica']);
-
-        Pizzas::create(['nombre' => 'Mariscos', 'precio' => '135', 'tipo' => 'especial']);
-        Pizzas::create(['nombre' => 'Queso Azul y Peras Caramelizadas', 'precio' => '125', 'tipo' => 'especial']);
-        Pizzas::create(['nombre' => 'Hongos', 'precio' => '110', 'tipo' => 'especial']);
-        Pizzas::create(['nombre' => 'Ciervo Ahumado', 'precio' => '140', 'tipo' => 'especial']);
-        Pizzas::create(['nombre' => 'Pollo', 'precio' => '105', 'tipo' => 'especial']);
+        Pizza::create(['nombre' => 'Jamón y Queso', 'precio' => '75', 'tipo' => 'clásica']);
+        Pizza::create(['nombre' => 'Fugazzeta', 'precio' => '85', 'tipo' => 'clásica']);
+        Pizza::create(['nombre' => 'Muzzarela', 'precio' => '90', 'tipo' => 'clásica']);
+        Pizza::create(['nombre' => 'Napolitana', 'precio' => '95', 'tipo' => 'clásica']);
+        Pizza::create(['nombre' => 'Roquefort', 'precio' => '85', 'tipo' => 'clásica']);
+        Pizza::create(['nombre' => 'Mariscos', 'precio' => '135', 'tipo' => 'especial']);
+        Pizza::create(['nombre' => 'Queso Azul y Peras Caramelizadas', 'precio' => '125', 'tipo' => 'especial']);
+        Pizza::create(['nombre' => 'Hongos', 'precio' => '110', 'tipo' => 'especial']);
+        Pizza::create(['nombre' => 'Ciervo Ahumado', 'precio' => '140', 'tipo' => 'especial']);
+        Pizza::create(['nombre' => 'Pollo', 'precio' => '105', 'tipo' => 'especial']);
     }
 
 }
